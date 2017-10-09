4f3afbe748a48d3157942cb99436dcfac96d137b
arminmrm <moharrer.a@husky..neu.edu>
2017-10-08 22:38:20 -0400

    Sieve of Eratosthenes code improved

--- Sieve_of_Eratosthenes.c
+++ Sieve_of_Eratosthenes.c
@@ -4,20 +4,27 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <pthread.h>
+#include <time.h>
 #include <semaphore.h>
-#define NUM 20
-#define DELTA 4
-#define segment_number NUM/DELTA
+#define NUM 1000000
+#define n_threads 16
+/*
 int a[NUM+1]={[2 ... NUM]=1,[1]=0,[0]=0};
+*/
 int done=0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 struct segment{
     int *arr;
-    int L;
+    int DELTA;
+    int index;
 };
 
-
+double CLOCK(){
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    return(t.tv_sec*1000)+(t.tv_nsec*1e-6);
+    }
 
 void regular_sieve(int *a,int N)
 {
@@ -32,29 +39,30 @@ void regular_sieve(int *a,int N)
           }
       }
 }
-void segment_sieve(int *arr,int L,int R)
+void segment_sieve(int *arr,int L,int R,int DELTA)
 {
     int i,j,m;
     for(i=2;i<DELTA+1;i++)
     {
         if(arr[i]==1)
           {
-              if(L>=(int)pow(i,2))
+              if((int)pow(i,2)>=L)
               {  
                    m=0;
               }
               else
               {
-                   m= (L-(int)pow(i,2))/i +1;
+                   m = (int)(ceil(((L-pow(i,2))/i)));
               }
-              for(j=(int)pow(i,2)+m*i;j<L+DELTA+1;j=j+i){
-                   a[j]=0;
+              for(j=(int)pow(i,2)+m*i;j<R+1;j=j+i){
+                   arr[j]=0;
                }
           }
     }
 }
 void print_primes(int * a,int N)
 {
+    printf("The prime numbers are:\n");
     int i;
     for(i= 2;i<N;i++){
         if(a[i] == 1){
@@ -67,11 +75,11 @@ void *first_sieve(void *arg)
     struct segment *my_segment=malloc(sizeof(struct segment*));
     my_segment = (struct segment*)arg;
     int *arr = my_segment->arr;
-    int L = my_segment->L;
+    int DELTA = my_segment->DELTA;
     regular_sieve(arr,DELTA);
     pthread_mutex_lock( &mutex );
     done++;
-    printf("The firsr segment is done so done is: %d\n",done);
+    printf("The first segment is done so done is: %d\n",done);
     pthread_cond_signal( &cond ); 
     pthread_mutex_unlock( & mutex );
 
@@ -82,14 +90,25 @@ void *next_sieve(void *arg)
     struct segment *my_segment=malloc(sizeof(struct segment*));
     my_segment = (struct segment*)arg;
     int *arr = my_segment->arr;
-    int L = my_segment->L;
+    int DELTA = my_segment->DELTA;
+    int i = my_segment->index;
+    int p_len = (NUM-DELTA)/n_threads;
     pthread_mutex_lock( &mutex );
     while(done <1)
     {
          pthread_cond_wait( & cond, & mutex ); 
     }
     pthread_mutex_unlock( &mutex );    
-    segment_sieve(arr,L,L+DELTA-1);
+    if(i < n_threads-1)
+        {
+        printf("Thread %d is working on numbers from %d to %d\n",i,DELTA+p_len*i+1,DELTA+p_len*(i+1));
+        segment_sieve(arr,DELTA+p_len*i+1,DELTA+p_len*(i+1),DELTA);
+        }
+    else
+        {
+        printf("Thread %d is working on numbers from %d to %d\n",i,DELTA+p_len*i+1,NUM);
+        segment_sieve(arr,DELTA+p_len*i+1,NUM,DELTA);
+        }
     return NULL;
 }
 int main()
@@ -99,22 +118,39 @@ int main()
     a[0] = 0;
     a[1]=0;
     */
-    printf("DELTA , segment num: %d %d\n",DELTA,segment_number);
+    double start,end;
+    int a[NUM+1]={[2 ... NUM]=1,[1]=0,[0]=0};
+    int DELTA = sqrt(NUM);
+    printf("Finding prime numbers up to %d, running with %d threads.\n",NUM,n_threads);
     pthread_t init_thread;
-    pthread_t next_threads[segment_number-1];
+    pthread_t next_threads[n_threads];
     int i;
     struct segment first_segment;
-    struct segment next_segments[segment_number-1];
+    struct segment next_segments[n_threads];
     first_segment.arr = a;
-    first_segment.L=2;
+    first_segment.index = 0;
+    first_segment.DELTA = DELTA;
+    start = CLOCK();
     pthread_create(&init_thread,NULL,first_sieve,(void *)&first_segment);
-    
-    for(i=0;i<segment_number-1;i++)
+    pthread_join(init_thread,NULL); 
+    for(i=0;i<n_threads;i++)
     {   
         next_segments[i].arr = a;
+        /*
         next_segments[i].L = (i+1)*DELTA+1;
+        */
+        next_segments[i].index = i;
+        next_segments[i].DELTA = DELTA;
         pthread_create(&next_threads[i],NULL,next_sieve,(void *)&next_segments[i]);
     }
-    pthread_join(init_thread,NULL);
+    
+    for(i=0;i<n_threads;i++)
+    { 
+        pthread_join(next_threads[i],NULL);
+    }
+    end = CLOCK();
+   /* 
     print_primes(a,NUM+1);
+    */
+    printf("Time taken is %f\n",end-start);
 }
